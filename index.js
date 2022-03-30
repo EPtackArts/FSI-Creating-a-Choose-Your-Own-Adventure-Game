bb54e5fc4003e618cdf1a1ff3ff9e1d8f493f8da
Elizabeth Ptack <elizabethptack@Mac-mini.local>
2022-03-30 17:34:42 -0500

    my first prompt

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 //Your Code here
+response = window.prompt("Hello!")
+
+if(response == "Hi", "Hello", "Howdy", "Hey", "Hiya", "Yo"){
+    window-prompt("Nice to meet you!")
+} else {
+    window-prompt("I wish you were nicer")
+}
